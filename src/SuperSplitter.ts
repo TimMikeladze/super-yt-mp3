ee73f62509c518838cf689c0b9dbc6ec75acf20a
TimMikeladze <tim.mikeladze@gmail.com>
2022-02-16 23:25:27 -0800

    feat: 🎸 improve downloading

--- src/SuperSplitter.ts
+++ src/SuperSplitter.ts
@@ -1,54 +1,71 @@
-import ytdl, { Chapter, chooseFormatOptions } from 'ytdl-core'
+import ytdl, { Chapter, chooseFormatOptions, videoInfo } from 'ytdl-core'
 import * as fs from 'fs'
 import * as path from 'path'
 import ffmpeg from 'fluent-ffmpeg'
 import getArtistTitle from 'get-artist-title'
 
 export interface SuperSplitterOptions {
+  url: string
 }
 
 export interface ExtendedChapter extends Chapter {
     end_time?: number
 }
 
-export const addChapterEndTimes = (lengthSeconds: number, chapters: Chapter[]): ExtendedChapter[] => {
-  if (chapters.length === 1) {
-    return [{
-      ...chapters[0],
-      end_time: lengthSeconds
-    }]
-  }
-  return chapters.map((x, index) => {
-    if (index === chapters.length - 1) {
-      return {
-        ...x,
-        end_time: lengthSeconds
-      }
-    } else {
-      return {
-        ...x,
-        end_time: chapters[index + 1].start_time
-      }
-    }
-  })
-}
-
 export class SuperSplitter {
   private readonly options: SuperSplitterOptions
+  private video: videoInfo
 
   constructor (options: SuperSplitterOptions) {
     this.options = options
   }
 
-  public async download (url: string, output: string, quality: chooseFormatOptions['quality']): Promise<void> {
-    const folderPath = path.resolve(output)
+  public async init (url: string): Promise<void> {
+    this.video = await ytdl.getInfo(url)
+  }
+
+  public async getArtistTitle (): Promise<string> {
+    const artistName = getArtistTitle(this.video.videoDetails.title)
+    return artistName?.[0]
+  }
+
+  public async getAlbumTitle (): Promise<string> {
+    const artistName = getArtistTitle(this.video.videoDetails.title)
+    return artistName?.[1] || this.video.videoDetails.title
+  }
+
+  private static addChapterEndTimes (lengthSeconds: number, chapters: Chapter[]): ExtendedChapter[] {
+    if (chapters.length === 1) {
+      return [{
+        ...chapters[0],
+        end_time: lengthSeconds
+      }]
+    }
+    return chapters.map((x, index) => {
+      if (index === chapters.length - 1) {
+        return {
+          ...x,
+          end_time: lengthSeconds
+        }
+      } else {
+        return {
+          ...x,
+          end_time: chapters[index + 1].start_time
+        }
+      }
+    })
+  }
+
+  public async download (artist: string, album: string, output: string, quality: chooseFormatOptions['quality'], format?: string): Promise<void> {
+    const folderPath = path.resolve(path.join(output, `${artist} - ${album}`))
+
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true })
+    }
+
     const tempFilePath = path.join(folderPath, 'temp.mp4')
 
-    const info = await ytdl.getInfo(url, {})
-
-    const artistName = getArtistTitle(info.videoDetails.title)
-
-    const chapters = addChapterEndTimes(Number(info.videoDetails.lengthSeconds), info.videoDetails.chapters)
+    const chapters = SuperSplitter.addChapterEndTimes(Number(this.video.videoDetails.lengthSeconds), this.video.videoDetails.chapters)
 
     const downloadVideo = async (url, path): Promise<void> => {
       return new Promise((resolve, reject) => {
@@ -65,7 +82,7 @@ export class SuperSplitter {
       })
     }
 
-    await downloadVideo(url, tempFilePath)
+    await downloadVideo(this.options.url, tempFilePath)
 
     await Promise.allSettled(chapters.map(async (chapter, index) => {
       return new Promise<void>((resolve, reject) => {
@@ -76,11 +93,25 @@ export class SuperSplitter {
           '-t', String(chapter.end_time - chapter.start_time)
         ]).on('error', err => {
           reject(err)
-        }).pipe(fs.createWriteStream(path.join(folderPath, `${artistName} - ${index + 1} - ${chapter.title}.mp3`)))
+        }).pipe(fs.createWriteStream(path.join(folderPath, `${SuperSplitter.formatTrackName(format, artist, album, index, chapter.title)}.mp3`)))
           .on('end', () => {
             resolve()
           })
       })
     }))
+
+    fs.unlinkSync(tempFilePath)
+  }
+
+  private static formatTrackName (format: string, artist: string, album: string, track: number, title: string): string {
+    if (!format) {
+      return `${track + 1} - ${title}`
+    }
+
+    return format
+      .replace('%artist%', artist)
+      .replace('%album%', album)
+      .replace('%track%', String(track + 1))
+      .replace('%title%', title)
   }
 }
--- src/cli.ts
+++ src/cli.ts
@@ -7,33 +7,53 @@ interface CommanderOptions {
   url: string
   quality?: string
   output: string
+  artist?: string
+  album?: string
+  format?: string
 }
 
-const program = new Command()
+(async () => {
+  const program = new Command()
 
-program.option('-o, --output <path>', 'output directory path')
-program.option('-q, --quality <quality>', 'URL of YT video')
-program.option('-u, --url <url>', 'URL of YT video')
+  program.option('-ar, --artist <artist>', 'Name of the artist(s). If not specified, the artist will be extracted from the title.')
+  program.option('-al --album <album>', 'Name of the album. If not specified, the album will be extracted from the title.')
+  program.option('-f, --format <format>', 'Naming format of tracks. For example `%artist% - %album% - %track% - %title%` or `%track - %title%`')
+  program.option('-o, --output <path>', 'Output directory path. Required.')
+  program.option('-q, --quality <quality>', 'URL of YouTube video. Required.')
+  program.option('-u, --url <url>', 'URL of YouTube video. Defaults to highest quality.')
 
-program.parse()
+  program.parse()
 
-program.parse(process.argv)
+  program.parse(process.argv)
 
-const options: CommanderOptions = program.opts()
+  const options: CommanderOptions = program.opts()
 
-if (!options.url) {
-  console.error('url is required')
-  process.exit(1)
-}
+  if (!options.url) {
+    console.error('url is required')
+    process.exit(1)
+  }
 
-if (!options.output) {
-  console.error('output directory path is required')
-  process.exit(1)
-}
+  if (!options.output) {
+    console.error('output directory path is required')
+    process.exit(1)
+  }
 
-const superSplitter = new SuperSplitter({
-})
+  const superSplitter = new SuperSplitter({
+    url: options.url
+  })
 
-superSplitter.download(options.url, options.output).then(() => {
-  process.exit(0)
-})
+  let artist = options.artist
+  let album = options.album
+
+  await superSplitter.init(options.url)
+
+  if (!artist) {
+    artist = await superSplitter.getArtistTitle()
+  }
+
+  if (!album) {
+    album = await superSplitter.getAlbumTitle()
+  }
+
+  await superSplitter.download(artist, album, options.output, options.quality, options.format)
+})()
